22a149c4e657ecbd916e978e72610e6f80fc8598
diegoloza94 <diegoloza94@gmail.com>
2021-05-15 20:08:59 +0100

    Screens

---
+++ navigators/root.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+// Screens
+import Login from "../screens/Login";
+import Signup from '../screens/Signup';
+import Home from "../screens/Home";
+
+// Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Custom styles
+import { colors } from "../shared/styles";
+
+const Stack = createStackNavigator();
+
+const Root = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitle: "SPLIT BILL TRIP",
+          headerStyle: {
+            backgroundColor: colors.primary,
+            shadowColor: "transparent",
+            shadowRadius: 0,
+            borderBottomWidth: 0,
+            elevation: 0,
+            shadowOffset: {
+                height: 0,
+            },
+            
+          },
+          headerTintColor: colors.secondary,
+          headerTitleStyle: {
+              letterSpacing: 1,
+              padding: 15,
+              fontSize: 15,
+          }
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen options={{headerMode: 'none', headerShown: false}} name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Root;
---
+++ screens/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+// UI elements
+import { View, Text, TouchableOpacity, Image } from "react-native";
+// Custom styles      <Image source={require("../assets/split.jpg")} style={basic.image} />
+import { basic, form } from "../shared/styles";
+
+const Home = ({ navigation }) => {
+  return (
+    <>
+      
+      <View style={basic.container}>
+        <View>
+          <View style={form.field}>
+            <Text style={form.heading}>Split the Bill of your Trip</Text>
+            <Text style={[form.text, { paddingVertical: 5, lineHeight: 25 }]}>
+             
+            </Text>
+          </View>
+          <View style={form.field}>
+            <TouchableOpacity style={form.button}>
+              <Text style={form.buttonText}> $ Calculate</Text>
+            </TouchableOpacity>
+            </View>
+            <View style={form.field}>
+            <TouchableOpacity onPress={() => navigation.navigate("Login")} style={form.button}>
+              <Text style={form.buttonText}> Log Out</Text>
+            </TouchableOpacity>
+            </View> 
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default Home;
